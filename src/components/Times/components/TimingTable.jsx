f894d219569cdb47f1efa7142bbf39b824c59a39
Luis Gomez <luis.gomez@bigcommerce.com>
2024-08-05 09:57:08 -0600

    add confirm modal on timingTable

--- src/components/Times/components/TimingTable.jsx
+++ src/components/Times/components/TimingTable.jsx
@@ -1,5 +1,7 @@
 import { useAuth } from "@/context/AuthContext";
 import React, { useState, useEffect } from "react";
+import Modal from "react-modal";
+import { ClipLoader } from "react-spinners";
 
 const TimingTable = () => {
   const [workers, setWorkers] = useState([]);
@@ -10,6 +12,10 @@ const TimingTable = () => {
   const [searchTerm, setSearchTerm] = useState("");
   const [socket, setSocket] = useState(null);
   const { username } = useAuth();
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [currentAction, setCurrentAction] = useState(null);
+  const [currentWorker, setCurrentWorker] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
 
   // Fetch worker data initially
   useEffect(() => {
@@ -89,7 +95,14 @@ const TimingTable = () => {
   };
 
   // Handle actions to start, pause, or confirm timing
-  const handleTimingAction = async (usuario, action) => {
+  const handleTimingAction = async (worker, action) => {
+    setCurrentAction(action);
+    setCurrentWorker(worker);
+    setModalIsOpen(true);
+  };
+
+  const confirmAction = async () => {
+    setIsLoading(true);
     const token = localStorage.getItem("token");
     try {
       const response = await fetch(
@@ -100,7 +113,11 @@ const TimingTable = () => {
             "Content-Type": "application/json",
             Authorization: `Bearer ${token}`,
           },
-          body: JSON.stringify({ usuario, action, username }),
+          body: JSON.stringify({
+            usuario: currentWorker.usuario,
+            action: currentAction,
+            username,
+          }),
         },
       );
 
@@ -111,6 +128,9 @@ const TimingTable = () => {
       }
     } catch (error) {
       console.error("Error updating timing status:", error);
+    } finally {
+      setIsLoading(false);
+      setModalIsOpen(false);
     }
   };
 
@@ -160,6 +180,19 @@ const TimingTable = () => {
     (a, b) => b.totalMinutes - a.totalMinutes,
   );
 
+  const actionText = (action) => {
+    switch (action) {
+      case "start":
+        return "iniciar";
+      case "pause":
+        return "pausar";
+      case "confirm":
+        return "cerrar";
+      default:
+        return action;
+    }
+  };
+
   return (
     <div>
       <input
@@ -203,10 +236,8 @@ const TimingTable = () => {
                   </td>
                   <td className="px-6 py-4">
                     <button
-                      onClick={() =>
-                        handleTimingAction(worker.usuario, "start")
-                      }
-                      className="text-blue-400 hover:underline"
+                      onClick={() => handleTimingAction(worker, "start")}
+                      className="rounded-lg bg-green-400 p-2  font-bold text-black hover:bg-green-600"
                     >
                       Iniciar
                     </button>
@@ -216,7 +247,7 @@ const TimingTable = () => {
             </tbody>
           </table>
         </div>
-        <div className="relative overflow-x-auto shadow-md dark:border-strokedark dark:bg-boxdark sm:rounded-lg">
+        <div className="relative h-100 overflow-x-auto shadow-md dark:border-strokedark dark:bg-boxdark sm:rounded-lg">
           <h2 className="mb-4 text-center text-lg font-bold">
             Trabajadores con tiempo activo
           </h2>
@@ -232,7 +263,11 @@ const TimingTable = () => {
                 <th scope="col" className="px-6 py-3">
                   Abierto Por
                 </th>
-                <th scope="col" className="px-6 py-3">
+                <th
+                  scope="col"
+                  className="px-6 py-3"
+                  style={{ width: "200px" }}
+                >
                   Acciones
                 </th>
               </tr>
@@ -259,18 +294,14 @@ const TimingTable = () => {
                   <td className="px-6 py-4">{worker.createdBy || "-"}</td>
                   <td className="px-6 py-4">
                     <button
-                      onClick={() =>
-                        handleTimingAction(worker.usuario, "pause")
-                      }
-                      className="mr-2 text-yellow-400 hover:underline"
+                      onClick={() => handleTimingAction(worker, "pause")}
+                      className="mr-2 rounded-lg bg-yellow-400 p-2 font-bold text-black hover:bg-yellow-600"
                     >
                       Pausar
                     </button>
                     <button
-                      onClick={() =>
-                        handleTimingAction(worker.usuario, "confirm")
-                      }
-                      className="text-green-400 hover:underline"
+                      onClick={() => handleTimingAction(worker, "confirm")}
+                      className=" mt-2 rounded-lg bg-rose-400 p-2 font-bold text-black hover:bg-rose-600"
                     >
                       Confirmar
                     </button>
@@ -296,7 +327,11 @@ const TimingTable = () => {
                 <th scope="col" className="px-6 py-3">
                   Pausado Por
                 </th>
-                <th scope="col" className="px-6 py-3">
+                <th
+                  scope="col"
+                  className="px-6 py-3"
+                  style={{ width: "200px" }}
+                >
                   Acciones
                 </th>
               </tr>
@@ -323,18 +358,14 @@ const TimingTable = () => {
                   <td className="px-6 py-4">{worker.createdBy || "-"}</td>
                   <td className="px-6 py-4">
                     <button
-                      onClick={() =>
-                        handleTimingAction(worker.usuario, "start")
-                      }
-                      className="mr-2 text-blue-400 hover:underline"
+                      onClick={() => handleTimingAction(worker, "start")}
+                      className="mr-2 rounded-lg bg-blue-400 p-2 font-bold text-black hover:bg-blue-600"
                     >
                       Reiniciar
                     </button>
                     <button
-                      onClick={() =>
-                        handleTimingAction(worker.usuario, "confirm")
-                      }
-                      className="text-green-400 hover:underline"
+                      onClick={() => handleTimingAction(worker, "confirm")}
+                      className=" mt-2 rounded-lg bg-rose-400 p-2 font-bold text-black hover:bg-rose-600"
                     >
                       Confirmar
                     </button>
@@ -345,6 +376,39 @@ const TimingTable = () => {
           </table>
         </div>
       </div>
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={() => setModalIsOpen(false)}
+        className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50"
+      >
+        <div className="rounded-md bg-white p-6 shadow-md">
+          <h2 className="mb-4 text-xl font-bold">Confirmar acción</h2>
+          <p>
+            ¿Estás seguro de que quieres {actionText(currentAction)} el tiempo
+            para {currentWorker?.usuario}?
+          </p>
+          {/* <p>Abierto por: {currentWorker?.createdBy}</p> */}
+          <div className="mt-6 flex justify-end gap-4">
+            <button
+              onClick={() => setModalIsOpen(false)}
+              className="text-gray-600 bg-gray-200 rounded-md px-4 py-2 text-sm"
+            >
+              Cancelar
+            </button>
+            <button
+              onClick={confirmAction}
+              className="flex items-center gap-2 rounded-md bg-blue-600 px-4 py-2 text-sm text-white"
+              disabled={isLoading}
+            >
+              {isLoading ? (
+                <ClipLoader size={20} color={"#ffffff"} />
+              ) : (
+                "Confirmar"
+              )}
+            </button>
+          </div>
+        </div>
+      </Modal>
     </div>
   );
 };
